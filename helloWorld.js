f6becbbbf02131b260f78264fbc48ebe28073c9f
Nathan <nmf2@cin.ufpe.br>
2021-08-23 11:32:00 -0300

    middlewares and password recovery

--- helloWorld.js
+++ helloWorld.js
@@ -1,15 +1,9 @@
-const axios = require('axios').default
 const express = require('express')
 const app = express()
 const port = 3000
 
 app.get('/', (req, res) => {
   res.send('Hello World!')
-})
-
-app.get('/affirmations', (req, res) => {
-  const affirmationsResponse = await axios.get('https://affirmations.dev')
-  res.send(affirmationsResponse.data)
 })
 
 app.listen(port, () => {
--- server.js
+++ server.js
@@ -1,15 +1,41 @@
 const express = require("express");
+const axios = require("axios").default;
+const cors = require("cors");
+const { json, urlencoded } = require("body-parser");
+
 const { firestore } = require("./firebase");
 const app = express();
+
 const port = 3000;
 
+// Middlewares
+app.use(json());
+app.use(urlencoded({ extended: true }));
+app.use(cors());
+
+app.use((req, res, next) => {
+  if (req.headers.authorization === "Bearer autenticado") {
+    next();
+  } else {
+    res.sendStatus(403);
+  }
+});
+
+// ------------ Rotas!
+// Affirmations
+app.get("/affirmations", async (req, res) => {
+  const affirmationsResponse = await axios.get("https://affirmations.dev");
+  res.send(affirmationsResponse.data);
+});
+
+// CRUD Users
 app.get("/users", async (req, res) => {
   try {
     const users = await firestore.collection("users").get();
     const usersData = users.docs.map((userDoc) => {
-      const user = userDoc.data()
-      delete user.password
-      return user
+      const user = userDoc.data();
+      delete user.password;
+      return user;
     });
     res.status(200).send(usersData);
   } catch (e) {
@@ -24,7 +50,7 @@ app.get("/users/:id", async (req, res) => {
 
     if (document.exists) {
       const userData = document.data();
-      delete userData.password
+      delete userData.password;
       res.status(200).send(userData);
     } else {
       res.status(404).send();
@@ -51,6 +77,7 @@ app.post("/users", async (req, res) => {
     const document = await firestore.collection("users").add(user);
     res.status(201).send({ id: document.id });
   } catch (e) {
+    console.error(e);
     res.status(500).send("Internal Error, Sorry");
   }
 });
@@ -61,8 +88,8 @@ app.put("/users/:id", async (req, res) => {
     const userData = {
       id: docId,
       name: req.body.name,
-      password: req.body.password
-    }
+      password: req.body.password,
+    };
     await firestore
       .collection("users")
       .doc(docId)
@@ -72,6 +99,28 @@ app.put("/users/:id", async (req, res) => {
     res.status(500).send("Internal Error, Sorry");
   }
 });
+
+app.post("/users/:id/recover-password", async (req, res) => {
+  try {
+    const docId = req.params.id;
+    let randomPassword = (Math.random() + 1).toString(36).substring(2);
+    const userData = {
+      password: randomPassword,
+    };
+
+    await firestore
+      .collection("users")
+      .doc(docId)
+      .set(userData, { merge: true });
+
+    console.log(`Manda email pro usuário com a senha dele: ${randomPassword}`);
+    res.status(200).send();
+  } catch (e) {
+    console.error(e)
+    res.status(500).send("Internal Error, Sorry");
+  }
+});
+
 
 app.listen(port, () => {
   console.log(`Example app listening at http://localhost:${port}`);
