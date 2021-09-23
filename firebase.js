48dbba141c69ceca1c2542444ac2b060609aa0b5
Nathan <nmf2@cin.ufpe.br>
2021-09-23 13:26:56 -0300

    feat: add check for service account

--- firebase.js
+++ firebase.js
@@ -1,4 +1,13 @@
 const admin = require("firebase-admin");
+const fs = require("fs");
+
+if (fs.existsSync("./firebase-service-account.json") === false) {
+  throw new Error(
+    `Adicione o JSON da Service Account do seu projeto do Firebase!
+  Acesse as Configurações do Projeto no Firebase, vá em Contas de Serviço, clique em Criar Conta de Serviço, gere uma chave, baixe ela, depois coloque nesse repositório com o nome firebase-servicea-account.json
+  Link: https://console.firebase.google.com/project/_/settings/serviceaccounts/adminsdk`
+  );
+}
 
 const serviceAccount = require("./firebase-service-account.json");
 
